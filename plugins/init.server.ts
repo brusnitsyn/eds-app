93762d6fec59514787e22d5befd478c5459fb4c2
brusnitsyn <brusnitsyn@github.com>
2024-08-16 15:21:46 +0900

    add value

--- plugins/init.server.ts
+++ plugins/init.server.ts
@@ -2,5 +2,5 @@ import { useSanctumAuth } from '../composables/useSanctumAuth'
 
 export default defineNuxtPlugin(async (nitroApp) => {
   const { cookieToken, refreshUser } = useSanctumAuth()
-  if (cookieToken !== null) { await refreshUser() }
+  if (cookieToken.value !== null) { await refreshUser() }
 })
