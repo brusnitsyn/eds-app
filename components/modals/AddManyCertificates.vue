f11157a99db7fe2c48dbe8ee60e078a1c330f028
brusnitsyn <brusnitsyn@github.com>
2024-09-21 19:25:22 +0900

    a

--- components/modals/AddManyCertificates.vue
+++ components/modals/AddManyCertificates.vue
@@ -16,7 +16,6 @@ async function uploadArchive({ file }: UploadCustomRequestOptions) {
 
   const { status } = await useAPI('/api/certificate/upload', {
     method: 'POST',
-    mode: 'no-cors',
     body: formData
   })
 
