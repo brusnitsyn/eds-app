ac33e71a0187d0b389f1800b61bec34a99d1a575
brusnitsyn <andrew.brusnitsyn@gmail.com>
2024-12-03 08:13:05 +0900

    u

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,20 +1,21 @@
 import { joinURL } from 'ufo'
 
 export default defineEventHandler(async (event) => {
-  const hostname = getRequestURL(event).hostname
-  let proxyUrl = ''
-  switch (hostname) {
-    case 'localhost':
-    case '127.0.0.1':
-      proxyUrl = 'http://localhost:81/api'
-      break
-    case '10.32.0.204':
-      proxyUrl = 'http://10.32.0.204:81/api'
-      break
-    case '46.16.9.130':
-      proxyUrl = 'http://localhost:81/api'
-      break
-  }
+  // const hostname = getRequestURL(event).hostname
+  const proxyUrl = 'http://localhost:81/api'
+  // switch (hostname) {
+  //   case 'localhost':
+  //   case '127.0.0.1':
+  //   case 'eds.amurokb.ru':
+  //     proxyUrl = 'http://localhost:81/api'
+  //     break
+  //   case '10.32.0.204':
+  //     proxyUrl = 'http://10.32.0.204:81/api'
+  //     break
+  //   case '46.16.9.130':
+  //     proxyUrl = 'http://localhost:81/api'
+  //     break
+  // }
 
   console.log('hostname', hostname)
 
